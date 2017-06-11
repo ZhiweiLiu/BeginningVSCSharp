d20efb34b908de3013bc9b90510da432933cfa83
zhiwei.liu <510492340@qq.com>
2017-06-11 22:47:49 +0800

    类和接口的定义

---
+++ Chapter09/Traffic/Traffic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    class Program
+    {
+        public static void AddPassenger(IPassengerCarrier vehicle)
+        {
+            Console.WriteLine(vehicle.ToString());
+        }
+        static void Main(string[] args)
+        {
+            AddPassenger(new PassengerTrain());
+            AddPassenger(new Compact());
+            AddPassenger(new SUV());
+        }
+    }
+}
